8722dfd74bc9d89138bc098e45c57adf0dcaaf64
GodCoder <mygodcoder@gmail.com>
2020-05-13 15:53:58 +0900

    init

---
+++ src/main/java/com/godcoder/myhome/MyhomeApplication.java
@@ -0,0 +1,13 @@
+package com.godcoder.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godcoder/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
