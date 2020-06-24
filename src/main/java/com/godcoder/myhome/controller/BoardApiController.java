5fa10ee6c983502738bc48dc0df795e86621db2a
godcoder <mygodcoder@gmail.com>
2020-06-24 14:02:51 +0900

    restful crud

---
+++ src/main/java/com/godcoder/myhome/controller/BoardApiController.java
@@ -0,0 +1,58 @@
+package com.godcoder.myhome.controller;
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository repository;
+
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+        @RequestParam(required = false, defaultValue = "") String content) {
+        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+            return repository.findAll();
+        } else {
+            return repository.findByTitleOrContent(title, content);
+        }
+    }
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    // Single item
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return repository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
--- src/main/java/com/godcoder/myhome/repository/BoardRepository.java
+++ src/main/java/com/godcoder/myhome/repository/BoardRepository.java
@@ -3,5 +3,11 @@ package com.godcoder.myhome.repository;
 import com.godcoder.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+
 }
