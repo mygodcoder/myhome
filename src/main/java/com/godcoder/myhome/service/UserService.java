0b1a5eb5bf2705ffbe26b29acf8d21e3113d5aea
cosin <playcosin@gmail.com>
2023-05-01 14:39:16 +0900

    트랜잭션 처리 추가

--- src/main/java/com/godcoder/myhome/service/UserService.java
+++ src/main/java/com/godcoder/myhome/service/UserService.java
@@ -1,11 +1,16 @@
 package com.godcoder.myhome.service;
 
+import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.model.Role;
 import com.godcoder.myhome.model.User;
+import com.godcoder.myhome.repository.BoardRepository;
 import com.godcoder.myhome.repository.UserRepository;
+import org.omg.SendingContext.RunTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -14,16 +19,33 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
+
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
     public User save(User user) {
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
         user.setEnabled(true);
+
         Role role = new Role();
         role.setId(1l);
         user.getRoles().add(role);
-        return userRepository.save(user);
+
+        User savedUser = userRepository.save(user);
+
+        // 사용자 가입인사글 자동작성
+        Board board = new Board();
+        board.setTitle("안녕하세요!");
+        board.setContent("반갑습니다.");
+        board.setUser(savedUser);
+        boardRepository.save(board);
+
+        return savedUser;
     }
 
 }
